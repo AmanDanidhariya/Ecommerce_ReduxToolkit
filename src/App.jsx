2a9036ea83b09834b2bb00c254d5e1d7f326b60e
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-07-17 17:29:51 +0530

    build a single product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import "./App.css";
 import Main from "./components/Main/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FilteredProducts from "./components/FilteredProducts/FilteredProducts";
+import SingleProduct from "./components/FilteredProducts/SingleProduct";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}  ></Route>
-        <Route path="/filteredProducts/:type" element={<FilteredProducts/>}></Route>
-      </Routes>
-        
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route
+            path="/filteredProducts/:type"
+            element={<FilteredProducts />}
+          ></Route>
+          <Route
+            path="/filteredProducts/:type/:id"
+            element={<SingleProduct />}
+          ></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/features/slices/productSlice.jsx
+++ src/features/slices/productSlice.jsx
@@ -7,6 +7,8 @@ export const productSlice = createSlice({
     //get item from session storage
     filteredProducts:
       JSON.parse(sessionStorage.getItem("filteredData")) || storeData,
+    singleProduct:
+      JSON.parse(sessionStorage.getItem("oneProduct")) || storeData,
   },
   reducers: {
     filterProducts(state, action) {
@@ -20,7 +22,19 @@ export const productSlice = createSlice({
         //storing in session storage
         const saveState = JSON.stringify(filter);
         sessionStorage.setItem("filteredData", saveState);
-        
+      } catch (err) {
+        return err;
+      }
+    },
+    singleProduct(state, action) {
+      try {
+        //when product.id match with action.payload return that product in single product page
+        const oneProduct = storeData.filter(
+          (product) => product.id === action.payload
+        );
+        state.singleProduct = oneProduct;
+        const saveState = JSON.stringify(oneProduct);
+        sessionStorage.setItem("oneProduct", saveState);
       } catch (err) {
         return err;
       }
@@ -28,5 +42,5 @@ export const productSlice = createSlice({
   },
 });
 
-export const { filterProducts } = productSlice.actions;
+export const { filterProducts, singleProduct } = productSlice.actions;
 export default productSlice.reducer;
